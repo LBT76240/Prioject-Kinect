1bf46a0b43409022632baf3e131d5da289696148
LBT76240 <trailin_88_@hotmail.fr>
2017-11-13 09:42:15 +0100

    Ajout d'un menu de démarrage

---
+++ Assets/DebugSouris.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugSouris : MonoBehaviour {
+    public GameObject right_hand;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = Input.mousePosition;
+        pos.z = right_hand.transform.position.z;
+        
+        
+
+        right_hand.transform.position = pos;
+
+    }
+}
--- Assets/Script/EstSelectionnableMVS.cs
+++ Assets/Script/EstSelectionnableMVS.cs
@@ -1,31 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class EstSelectionnableMVS : MonoBehaviour {
 
     public GameObject right_hand;
     public Material material;
+    public Text text;
+    public Slider slider;
     public float erreur;
+    public float timer;
+    public float timerLimit = 3f;
+    public string sceneToLoad;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        timer = 0f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	    if(pasloin(erreur)) {
-            Color color = material.color;
-            color.r = 1;
-            material.color = color;
+            
             print("Proche");
+            timer += Time.deltaTime;
+            if(timer> timerLimit) {
+                timer = timerLimit;
+                Application.LoadLevel(sceneToLoad);
+            }
         } else {
-            Color color = material.color;
-            color.r = 0;
-            material.color = color;
+ 
             print("Loin");
+            timer -= Time.deltaTime;
+            if (timer < 0f) {
+                timer = 0f;
+            }
         }
-	}
+        Color color = material.color;
+        color.g = timer / timerLimit;
+        color.r = 1-timer / timerLimit;
+        material.color = color;
+        slider.value= timer / timerLimit;
+    }
 
     bool pasloin(float erreur) {
         float distance = Mathf.Pow(right_hand.transform.position.x - transform.position.x,2) + Mathf.Pow(right_hand.transform.position.y - transform.position.y,2);
